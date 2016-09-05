4f7b4424e8482b8ad5d19cfd5b6873bf09bb6df1
Jean-François Verdon <jf.verdon@mgdesign.fr>
2016-09-05 14:45:18 +0200

    Extracting playback sample logic into pseudo C functions

--- samples/playback/sample_playback.cc
+++ samples/playback/sample_playback.cc
@@ -67,121 +67,8 @@ OZZ_OPTIONS_DECLARE_STRING(
   "media/arnaud_mesh.ozz",
   false)
 
-class LoadSampleApplication : public ozz::sample::Application {
- public:
-  LoadSampleApplication()
-    : cache_(NULL) {
-  }
-
- protected:
-  // Updates current animation time.
-  virtual bool OnUpdate(float _dt) {
-    // Updates current animation time.
-    controller_.Update(animation_, _dt);
-
-    // Samples optimized animation at t = animation_time_.
-    ozz::animation::SamplingJob sampling_job;
-    sampling_job.animation = &animation_;
-    sampling_job.cache = cache_;
-    sampling_job.time = controller_.time();
-    sampling_job.output = locals_;
-    if (!sampling_job.Run()) {
-      return false;
-    }
-
-    // Converts from local space to model space matrices.
-    ozz::animation::LocalToModelJob ltm_job;
-    ltm_job.skeleton = &skeleton_;
-    ltm_job.input = locals_;
-    ltm_job.output = models_;
-    if (!ltm_job.Run()) {
-      return false;
-    }
-
-    // Update skinning matrices latest blending stage output.
-    assert(models_.Count() == skinning_matrices_.Count() &&
-           models_.Count() == mesh_.inverse_bind_poses.size());
-
-    // Builds skinning matrices, based on the output of the animation stage.
-    for (size_t i = 0; i < models_.Count(); ++i) {
-      skinning_matrices_[i] = models_[i] * mesh_.inverse_bind_poses[i];
-    }
-
-    return true;
-  }
-
-  // Samples animation, transforms to model space and renders.
-  virtual bool OnDisplay(ozz::sample::Renderer* _renderer) {
-    return _renderer->DrawSkinnedMesh(mesh_,
-                                      skinning_matrices_,
-                                      ozz::math::Float4x4::identity());
-  }
-
-  virtual bool OnInitialize() {
-    ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
-
-    // Reading skeleton.
-    if (!ozz::sample::LoadSkeleton(OPTIONS_skeleton, &skeleton_)) {
-        return false;
-    }
-
-    // Reading animation.
-    if (!ozz::sample::LoadAnimation(OPTIONS_animation, &animation_)) {
-        return false;
-    }
-    
-    const int num_joints = skeleton_.num_joints();
-
-    // Reading skinned mesh.
-    if (!ozz::sample::LoadMesh(OPTIONS_mesh, &mesh_)) {
-      return false;
-    }
-
-    // The number of joints of the mesh needs to match skeleton.
-    if (mesh_.num_joints() != num_joints) {
-      ozz::log::Err() << "The provided mesh doesn't match skeleton "
-        "(joint count mismatch)." << std::endl;
-      return false;
-    }
-
-    // Allocates runtime buffers.
-    const int num_soa_joints = skeleton_.num_soa_joints();
-    locals_ = allocator->AllocateRange<ozz::math::SoaTransform>(num_soa_joints);
-    models_ = allocator->AllocateRange<ozz::math::Float4x4>(num_joints);
-    skinning_matrices_ = allocator-> AllocateRange<ozz::math::Float4x4>(num_joints);
-
-    // Allocates a cache that matches animation requirements.
-    cache_ = allocator->New<ozz::animation::SamplingCache>(num_joints);
-
-    return true;
-  }
-
-  virtual void OnDestroy() {
-    ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
-    allocator->Deallocate(locals_);
-    allocator->Deallocate(models_);
-    allocator->Deallocate(skinning_matrices_);
-    allocator->Delete(cache_);
-  }
-
-  virtual bool OnGui(ozz::sample::ImGui* _im_gui) {
-    // Exposes animation runtime playback controls.
-    {
-      static bool open = true;
-      ozz::sample::ImGui::OpenClose oc(_im_gui, "Animation control", &open);
-      if (open) {
-        controller_.OnGui(animation_, _im_gui);
-      }
-    }
-    return true;
-  }
-
-  virtual void GetSceneBounds(ozz::math::Box* _bound) const {
-    ozz::sample::ComputePostureBounds(models_, _bound);
-  }
-
- private:
-
+struct Data
+{
   // Playback animation controller. This is a utility class that helps with
   // controlling animation playback time.
   ozz::sample::PlaybackController controller_;
@@ -207,6 +94,144 @@ class LoadSampleApplication : public ozz::sample::Application {
   // Buffer of skinning matrices, result of the joint multiplication of the
   // inverse bind pose with the model space matrix.
   ozz::Range<ozz::math::Float4x4> skinning_matrices_;
+};
+
+bool initialize(Data* data)
+{
+  ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
+  data->cache_ = NULL;
+
+  // Reading skeleton.
+  if (!ozz::sample::LoadSkeleton(OPTIONS_skeleton, &data->skeleton_)) {
+      return false;
+  }
+
+  // Reading animation.
+  if (!ozz::sample::LoadAnimation(OPTIONS_animation, &data->animation_)) {
+      return false;
+  }
+    
+  const int num_joints = data->skeleton_.num_joints();
+
+  // Reading skinned mesh.
+  if (!ozz::sample::LoadMesh(OPTIONS_mesh, &data->mesh_)) {
+    return false;
+  }
+
+  // The number of joints of the mesh needs to match skeleton.
+  if (data->mesh_.num_joints() != num_joints) {
+    ozz::log::Err() << "The provided mesh doesn't match skeleton "
+      "(joint count mismatch)." << std::endl;
+    return false;
+  }
+
+  // Allocates runtime buffers.
+  const int num_soa_joints = data->skeleton_.num_soa_joints();
+  data->locals_ = allocator->AllocateRange<ozz::math::SoaTransform>(num_soa_joints);
+  data->models_ = allocator->AllocateRange<ozz::math::Float4x4>(num_joints);
+  data->skinning_matrices_ = allocator-> AllocateRange<ozz::math::Float4x4>(num_joints);
+
+  // Allocates a cache that matches animation requirements.
+  data->cache_ = allocator->New<ozz::animation::SamplingCache>(num_joints);
+  return true;
+}
+
+void dispose(Data* data)
+{
+    ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
+    allocator->Deallocate(data->locals_);
+    allocator->Deallocate(data->models_);
+    allocator->Deallocate(data->skinning_matrices_);
+    allocator->Delete(data->cache_);
+    data->cache_ = NULL;
+}
+
+bool update(Data* data, float _dt)
+{
+  // Updates current animation time.
+  data->controller_.Update(data->animation_, _dt);
+
+  // Samples optimized animation at t = animation_time_.
+  ozz::animation::SamplingJob sampling_job;
+  sampling_job.animation = &data->animation_;
+  sampling_job.cache = data->cache_;
+  sampling_job.time = data->controller_.time();
+  sampling_job.output = data->locals_;
+  if (!sampling_job.Run()) {
+    return false;
+  }
+
+  // Converts from local space to model space matrices.
+  ozz::animation::LocalToModelJob ltm_job;
+  ltm_job.skeleton = &data->skeleton_;
+  ltm_job.input = data->locals_;
+  ltm_job.output = data->models_;
+  if (!ltm_job.Run()) {
+    return false;
+  }
+
+  // Update skinning matrices latest blending stage output.
+  assert(data->models_.Count() == data->skinning_matrices_.Count() &&
+         data->models_.Count() == data->mesh_.inverse_bind_poses.size());
+
+  // Builds skinning matrices, based on the output of the animation stage.
+  for (size_t i = 0; i < data->models_.Count(); ++i) {
+    data->skinning_matrices_[i] = data->models_[i] * data->mesh_.inverse_bind_poses[i];
+  }
+
+  return true;
+}
+
+bool render(Data* data, ozz::sample::Renderer* _renderer)
+{
+    return _renderer->DrawSkinnedMesh(data->mesh_,
+                                      data->skinning_matrices_,
+                                      ozz::math::Float4x4::identity());
+}
+
+class LoadSampleApplication : public ozz::sample::Application {
+ public:
+  LoadSampleApplication()
+  {
+  }
+
+ protected:
+  // Updates current animation time.
+  virtual bool OnUpdate(float _dt) {
+    return update(&data, _dt);
+  }
+
+  // Samples animation, transforms to model space and renders.
+  virtual bool OnDisplay(ozz::sample::Renderer* _renderer) {
+    return render(&data, _renderer);
+  }
+
+  virtual bool OnInitialize() {
+    return initialize(&data);
+  }
+
+  virtual void OnDestroy() {
+    dispose(&data);
+  }
+
+  virtual bool OnGui(ozz::sample::ImGui* _im_gui) {
+    // Exposes animation runtime playback controls.
+    {
+      static bool open = true;
+      ozz::sample::ImGui::OpenClose oc(_im_gui, "Animation control", &open);
+      if (open) {
+        data.controller_.OnGui(data.animation_, _im_gui);
+      }
+    }
+    return true;
+  }
+
+  virtual void GetSceneBounds(ozz::math::Box* _bound) const {
+    ozz::sample::ComputePostureBounds(data.models_, _bound);
+  }
+
+ private:
+    Data data;
 };
 
 int main(int _argc, const char** _argv) {
