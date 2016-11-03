a7db2649592fffcf39c267132fb140555ad08ddc
Jean-François Verdon <jf.verdon@mgdesign.fr>
2016-11-03 15:21:43 +0100

    CFFI warning

--- capi/cwrapper.h
+++ capi/cwrapper.h
@@ -81,10 +81,7 @@ OZZ_ANIMATION_C_API void update(struct Data* data, float _dt);
 OZZ_ANIMATION_C_API void setEntityTransform(struct Data* data, unsigned int entityId, float* transform);
 
 //-----------------------------------------------------------------------------
-#if CAPI_NO_SHADER
 OZZ_ANIMATION_C_API void render(struct Data* data, float* viewProjMatrix, int position_attrib, int normal_attrib, int uv_attrib, int u_model_matrix, int u_view_projection_matrix, int u_texture);
-#else
-OZZ_ANIMATION_C_API void render(struct Data* data, float* viewProjMatrix);
-#endif
+// OZZ_ANIMATION_C_API void render(struct Data* data, float* viewProjMatrix); // CAPI_NO_SHADER
 
 #endif // OZZ_C_WRAPPER
